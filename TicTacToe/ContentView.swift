ac780794f22de1b7fea2fda7fb1b23f1fa8c8de8
AMStudent <AMStudent@RS-F251-M19.local>
2021-09-15 10:14:27 -0500

    3D Effects works

--- TicTacToe/ContentView.swift
+++ TicTacToe/ContentView.swift
@@ -47,6 +47,13 @@ struct Home: View{
                     }
                         .frame(width: getWitdh(), height: getWitdh())
                         .cornerRadius(15)
+                    .rotation3DEffect(
+                        .init(degrees: moves[index] != "" ? 180: 0),
+                        axis: (x: 0.0, y: 1.0, 0.0),
+                        anchor: .center,
+                        anchorZ: 0.0,
+                        perspective: 1.0
+                    )
                     
                     .onTapGesture(perform: {
                         withAnimation(Animation.easeIn(duration: 0.5)) {
