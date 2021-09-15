c5341904365248c51879e8ca0bb21ebc802d2e82
AMStudent <AMStudent@RS-F251-M19.local>
2021-09-15 08:49:04 -0500

    Grid and Header placed

---
+++ TicTacToe/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  TicTacToe
+//
+//  Created by AMStudent on 9/15/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        NavigationView {
+        Home()
+            .navigationTitle("Tic Tac Toe")
+            .preferredColorScheme(.dark)
+        }
+    }
+}
+
+struct Home: View{
+    var body: some View {
+        VStack{
+            
+            LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 15), count: 3), spacing: 15) {
+                
+                ForEach(0..<9, id: \.self) {
+                    index in
+                    
+                    Color.white
+                }
+             }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
